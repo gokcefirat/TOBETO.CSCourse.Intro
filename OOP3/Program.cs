60b1b5a5caa6c53239f0c4579b8604212aae7f51
gokcefirat <gokcealp89@gmail.com>
2023-11-10 00:55:16 +0300

    OOP3

---
+++ OOP3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using OOP3;
+
+IKrediManager ihtiyacKrediManager =new IhtiyacKrediManager();
+
+//tasit-konut yada ihtiyac kredi manager demek yerine IKrediManager de veriyi getirir.
+
+TasitKrediManager tasitKrediManager = new TasitKrediManager();
+
+KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+ILoggerService databaseLoggerService = new DatabaseLoggerService();
+ILoggerService fileLoggerService = new FileLoggerService();
+
+
+
+BasvuruManager basvuruManager= new BasvuruManager();
+basvuruManager.BasvuruYap(ihtiyacKrediManager, new DatabaseLoggerService());
+
+List <IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager};
+
+//basvuruManager.KrediOnBilgilendirmesiYap(krediler);
